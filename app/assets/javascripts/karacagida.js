bb56422c81627182173dcdca801396b3467800e5
mtaner <mervetaner88@gmail.com>
2017-04-18 23:18:07 +0100

    Image pop-up on certificates

---
+++ app/assets/javascripts/karacagida.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".pop-up").on("click", function(event) {
+		event.preventDefault();
+		var id = $(this)[0].firstElementChild.id;
+ 		$('#imagepreview').attr('src', $('#' + id).attr('src'));
+		$('#imagemodal').modal('show');
+	});
+});
