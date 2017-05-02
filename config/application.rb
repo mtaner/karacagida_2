1dd9075f0488df4aa749c5c5fa09e3dadc08f135
mtaner <merve.taner@simplybusiness.co.uk>
2017-05-02 18:51:34 +0100

    Remove psql dependency

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,16 @@
 require_relative 'boot'
 
-require 'rails/all'
-
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+# require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -9,6 +18,7 @@ Bundler.require(*Rails.groups)
 module Karacagida2
   class Application < Rails::Application
     config.i18n.default_locale = :tr
+ 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
